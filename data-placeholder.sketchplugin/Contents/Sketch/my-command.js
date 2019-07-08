bb289e6deba372e9279d5264c67c2daeb71c5bde
Stephen Hathaway <stphnhthwy@gmail.com>
2019-07-08 11:16:12 -0600

    Plugin

---
+++ data-placeholder.sketchplugin/Contents/Sketch/my-command.js
@@ -0,0 +1,180 @@
+var that = this;
+function __skpm_run (key, context) {
+  that.context = context;
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/my-command.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/address.json":
+/*!**************************!*\
+  !*** ./src/address.json ***!
+  \**************************/
+/*! exports provided: address, default */
+/***/ (function(module) {
+
+module.exports = {"address":["2421 N 65 St Fort Lauderdale, FL 33308","1800 N Andrews Ave Fort Lauderdale, FL 33311","1423 Holly Heights Dr Fort Lauderdale, FL 33304","1470 N Dixie Hwy Fort Lauderdale, FL 33304","1839 Middle River Dr Fort Lauderdale, FL 33305","1332 Bayview Dr Fort Lauderdale, FL 33062","3200 Port Royale Dr Fort Lauderdale, FL 33308","3101 Seaside Dr Fort Lauderdale, FL 33308","3430 Galt Ocean Dr Fort Lauderdale, FL 33308"]};
+
+/***/ }),
+
+/***/ "./src/my-command.js":
+/*!***************************!*\
+  !*** ./src/my-command.js ***!
+  \***************************/
+/*! exports provided: onStartup, onShutdown, onSupplyAddress */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onStartup", function() { return onStartup; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onShutdown", function() { return onShutdown; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "onSupplyAddress", function() { return onSupplyAddress; });
+var sketch = __webpack_require__(/*! sketch */ "sketch");
+
+var DataSupplier = sketch.DataSupplier;
+
+var util = __webpack_require__(/*! util */ "util");
+
+var addressData = __webpack_require__(/*! ./address.json */ "./src/address.json");
+
+function onStartup() {
+  // To register the plugin, uncomment the relevant type:
+  DataSupplier.registerDataSupplier('public.text', 'Address', 'SupplyAddress'); // DataSupplier.registerDataSupplier('public.image', 'hello-data', 'SupplyData')
+}
+function onShutdown() {
+  DataSupplier.deregisterDataSuppliers();
+}
+function onSupplyAddress(context) {
+  var dataKey = context.data.key;
+  var dataCount = context.data.requestedCount;
+  var items = util.toArray(context.data.items).map(sketch.fromNative);
+  items.forEach(function (_, index) {
+    var address = addressData.address;
+    var addressIndex = Math.floor(Math.random() * address.length);
+    var data = address[addressIndex];
+    DataSupplier.supplyDataAtIndex(dataKey, data, index);
+  });
+}
+
+/***/ }),
+
+/***/ "sketch":
+/*!*************************!*\
+  !*** external "sketch" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ }),
+
+/***/ "util":
+/*!***********************!*\
+  !*** external "util" ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("util");
+
+/***/ })
+
+/******/ });
+  if (key === 'default' && typeof exports === 'function') {
+    exports(context);
+  } else {
+    exports[key](context);
+  }
+}
+that['onStartup'] = __skpm_run.bind(this, 'onStartup');
+that['onShutdown'] = __skpm_run.bind(this, 'onShutdown');
+that['onSupplyAddress'] = __skpm_run.bind(this, 'onSupplyAddress');
+that['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=my-command.js.map
